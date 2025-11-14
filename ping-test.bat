c57b561caa9c2f76ccf4512aa036719938efdc06
JavaddZiyaei <javadd.ziyaei@gmail.com>
2025-11-14 12:43:31 +0330

    Add files via upload

---
+++ ping-test.bat
@@ -0,0 +1,39 @@
+@echo off
+title TadbirNetwork Ping Monitor
+
+:: Set blue color
+color 1F
+
+echo ===============================================
+echo   TadbirNetwork (Network, Software, Hardware)
+echo   09135289377 (Mohandes Zaiae)
+echo ===============================================
+echo.
+
+set IP1=8.8.8.8
+set IP2=4.2.2.4
+
+:START
+echo Checking %IP1% ...
+ping -n 1 %IP1% >nul
+
+if errorlevel 1 (
+    echo Timeout or Connection Lost to %IP1%
+    echo Switching to %IP2%
+    echo.
+    goto PING2
+) else (
+    goto PING1
+)
+
+:PING1
+echo Ping Started on %IP1% ...
+ping %IP1%
+echo.
+goto START
+
+:PING2
+echo Ping Started on %IP2% ...
+ping %IP2%
+echo.
+goto START
